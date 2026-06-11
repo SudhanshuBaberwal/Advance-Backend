4c48a4eeb26d276bff0ef89bbd35f756231f2034
Sudhanshu Baberwal <24bcs147@iiitdwd.ac.in>
2026-06-11 15:26:32 +0530

    learn mircoservices

---
+++ level3/phase2/backend/services/product/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+
+const app = express()
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.get("/" , (req , res) => {
+    return res.status(200).json({message : "Hello from product services"})
+})
+
+app.listen(port , () => {
+    console.log("server started on port : " , port)
+})
